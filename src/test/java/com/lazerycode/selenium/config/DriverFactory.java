5ba70909e23917dd98fff3155370e57063239513
root <root@onekube-ip-192-168-122-22.localdomain>
2020-05-05 10:31:53 +0000

    test set up

--- src/test/java/com/lazerycode/selenium/config/DriverFactory.java
+++ src/test/java/com/lazerycode/selenium/config/DriverFactory.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static com.lazerycode.selenium.config.DriverType.FIREFOX;
+import static com.lazerycode.selenium.config.DriverType.CHROME;
 import static com.lazerycode.selenium.config.DriverType.valueOf;
 import static org.openqa.selenium.Proxy.ProxyType.MANUAL;
 import static org.openqa.selenium.remote.CapabilityType.PROXY;
@@ -29,7 +29,8 @@ public class DriverFactory {
     private final String proxyDetails = String.format("%s:%d", proxyHostname, proxyPort);
 
     public DriverFactory() {
-        DriverType driverType = FIREFOX;
+        //DriverType driverType = FIREFOX;
+        DriverType driverType = CHROME;
         String browser = System.getProperty("browser", driverType.toString()).toUpperCase();
         try {
             driverType = valueOf(browser);
---
+++ src/test/java/com/lazerycode/selenium/tests/loginIT.java
@@ -0,0 +1,31 @@
+// package test.java.com.lazerycode.selenium.tests;
+
+// import com.lazerycode.selenium.DriverBase;
+
+// /**
+//  * loginIT
+//  */
+
+
+// public class loginIT extends DriverBase{
+
+//     private String base_url = "localhost:31000";
+
+
+
+//     //# test case #1 Access Pivotal Bank web application root url, Login page loads
+//     @Test
+//     public void test_home_page(){
+//         WebDriver driver = getDriver();
+
+//         driver.get(base_url);
+
+//         // http status code check 
+//         // check if the loaded page has a login element with a form having a user name and password field
+
+//         //WebElement login = driver.
+
+//     }
+
+    
+// }
